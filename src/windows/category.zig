9c069e9599c0022cec8bd8690f681bd43c322c31
mkpoli <git@mkpo.li>
2024-04-02 19:24:30 +0900

    Use ITfCategoryMgr in win32

--- src/windows/category.zig
+++ src/windows/category.zig
@@ -4,147 +4,23 @@ const windows = std.os.windows;
 const WINAPI = windows.WINAPI;
 const HRESULT = windows.HRESULT;
 
-const com = @import("com.zig");
 const Guid = win32.zig.Guid;
 const CoCreateInstance = com.CoCreateInstance;
-pub const CLSID_TF_InputProcessorProfiles = com.CLSID_TF_InputProcessorProfiles;
-pub const IID_TF_InputProcessorProfiles = com.IID_ITfInputProcessorProfiles;
 
 const win32 = @import("win32");
-
+const com = win32.system.com;
+const ts = win32.ui.text_services;
 const IUnknown = win32.system.com.IUnknown;
 
-pub const ITfCategoryMgr = extern struct {
-    pub const IID = com.IID_ITfCategoryMgr;
+const CLSCTX_INPROC_SERVER = com.CLSCTX_INPROC_SERVER;
 
-    pub const VTable = extern struct {
-        base: IUnknown.VTable,
-        RegisterCategory: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, rclsid: *const Guid, rcatid: *const Guid, rguid: *const Guid) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, rclsid: *const Guid, rcatid: *const Guid, rguid: *const Guid) callconv(WINAPI) HRESULT,
-        },
-        UnregisterCategory: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, rclsid: *const Guid, rcatid: *const Guid, rguid: *const Guid) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, rclsid: *const Guid, rcatid: *const Guid, rguid: *const Guid) callconv(WINAPI) HRESULT,
-        },
-        EnumCategoriesInItem: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, rguid: *const Guid) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, rguid: *const Guid) callconv(WINAPI) HRESULT,
-        },
-        EnumItemsInCategory: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, rcatid: *const Guid) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, rcatid: *const Guid) callconv(WINAPI) HRESULT,
-        },
-        FindClosestCategory: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, rguid: *const Guid, pcatid: *Guid, ppcatidlist: *const *const Guid) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, rguid: *const Guid, pcatid: *Guid, ppcatidlist: *const *const Guid) callconv(WINAPI) HRESULT,
-        },
-        RegisterGUIDDescription: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, rclsid: *const Guid, rguid: *const Guid, pchdesc: ?*const u16) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, rclsid: *const Guid, rguid: *const Guid, pchdesc: ?*const u16) callconv(WINAPI) HRESULT,
-        },
-        RemoveGUIDDescription: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, rclsid: *const Guid, rguid: *const Guid) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, rclsid: *const Guid, rguid: *const Guid) callconv(WINAPI) HRESULT,
-        },
-        GetGUIDDescription: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, rguid: *const Guid) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, rguid: *const Guid) callconv(WINAPI) HRESULT,
-        },
-        RegisterGUIDDWORD: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, rclsid: *const Guid, rguid: *const Guid, dw: u32) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, rclsid: *const Guid, rguid: *const Guid, dw: u32) callconv(WINAPI) HRESULT,
-        },
-        UnregisterGUIDDWORD: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, rclsid: *const Guid, rguid: *const Guid) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, rclsid: *const Guid, rguid: *const Guid) callconv(WINAPI) HRESULT,
-        },
-        GetGUIDDWORD: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, rguid: *const Guid) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, rguid: *const Guid) callconv(WINAPI) HRESULT,
-        },
-        RegisterGUID: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, rguid: *const Guid) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, rguid: *const Guid) callconv(WINAPI) HRESULT,
-        },
-        GetGUID: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, guidatom: u32) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, guidatom: u32) callconv(WINAPI) HRESULT,
-        },
-        IsEqualTfGuidAtom: switch (@import("builtin").zig_backend) {
-            .stage1 => fn (self: *const ITfCategoryMgr, guidatom: u32, rguid: *const Guid) callconv(WINAPI) HRESULT,
-            else => *const fn (self: *const ITfCategoryMgr, guidatom: u32, rguid: *const Guid) callconv(WINAPI) HRESULT,
-        },
-    };
-
-    vtable: *const VTable,
-
-    pub fn MethodMixin(comptime T: type) type {
-        return struct {
-            pub usingnamespace IUnknown.MethodMixin(T);
-
-            pub inline fn ITfCategoryMgr_RegisterCategory(self: *const T, rclsid: *const Guid, rcatid: *const Guid, rguid: *const Guid) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).RegisterCategory(@as(*const ITfCategoryMgr, @ptrCast(self)), rclsid, rcatid, rguid);
-            }
-
-            pub inline fn ITfCategoryMgr_UnregisterCategory(self: *const T, rclsid: *const Guid, rcatid: *const Guid, rguid: *const Guid) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).UnregisterCategory(@as(*const ITfCategoryMgr, @ptrCast(self)), rclsid, rcatid, rguid);
-            }
-
-            pub inline fn ITfCategoryMgr_EnumCategoriesInItem(self: *const T, rguid: *const Guid) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).EnumCategoriesInItem(@as(*const ITfCategoryMgr, @ptrCast(self)), rguid);
-            }
-
-            pub inline fn ITfCategoryMgr_EnumItemsInCategory(self: *const T, rcatid: *const Guid) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).EnumItemsInCategory(@as(*const ITfCategoryMgr, @ptrCast(self)), rcatid);
-            }
-
-            pub inline fn ITfCategoryMgr_FindClosestCategory(self: *const T, rguid: *const Guid, pcatid: *Guid, ppcatidlist: *const *const Guid) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).FindClosestCategory(@as(*const ITfCategoryMgr, @ptrCast(self)), rguid, pcatid, ppcatidlist);
-            }
-
-            pub inline fn ITfCategoryMgr_RegisterGUIDDescription(self: *const T, rclsid: *const Guid, rguid: *const Guid, pchdesc: ?*const u16) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).RegisterGUIDDescription(@as(*const ITfCategoryMgr, @ptrCast(self)), rclsid, rguid, pchdesc);
-            }
-
-            pub inline fn ITfCategoryMgr_RemoveGUIDDescription(self: *const T, rclsid: *const Guid, rguid: *const Guid) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).RemoveGUIDDescription(@as(*const ITfCategoryMgr, @ptrCast(self)), rclsid, rguid);
-            }
-
-            pub inline fn ITfCategoryMgr_GetGUIDDescription(self: *const T, rguid: *const Guid) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).GetGUIDDescription(@as(*const ITfCategoryMgr, @ptrCast(self)), rguid);
-            }
-
-            pub inline fn ITfCategoryMgr_RegisterGUIDDWORD(self: *const T, rclsid: *const Guid, rguid: *const Guid, dw: u32) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).RegisterGUIDDWORD(@as(*const ITfCategoryMgr, @ptrCast(self)), rclsid, rguid, dw);
-            }
-
-            pub inline fn ITfCategoryMgr_UnregisterGUIDDWORD(self: *const T, rclsid: *const Guid, rguid: *const Guid) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).UnregisterGUIDDWORD(@as(*const ITfCategoryMgr, @ptrCast(self)), rclsid, rguid);
-            }
-
-            pub inline fn ITfCategoryMgr_GetGUIDDWORD(self: *const T, rguid: *const Guid) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).GetGUIDDWORD(@as(*const ITfCategoryMgr, @ptrCast(self)), rguid);
-            }
-
-            pub inline fn ITfCategoryMgr_RegisterGUID(self: *const T, rguid: *const Guid) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).RegisterGUID(@as(*const ITfCategoryMgr, @ptrCast(self)), rguid);
-            }
-
-            pub inline fn ITfCategoryMgr_GetGUID(self: *const T, guidatom: u32) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).GetGUID(@as(*const ITfCategoryMgr, @ptrCast(self)), guidatom);
-            }
-
-            pub inline fn ITfCategoryMgr_IsEqualTfGuidAtom(self: *const T, guidatom: u32, rguid: *const Guid) HRESULT {
-                return @as(*const ITfCategoryMgr.VTable, @ptrCast(self.vtable)).IsEqualTfGuidAtom(@as(*const ITfCategoryMgr, @ptrCast(self)), guidatom, rguid);
-            }
-        };
-    }
-    pub usingnamespace MethodMixin(@This());
-};
-
+pub const ITfCategoryMgr = ts.ITfCategoryMgr;
+const IID_ITfCategoryMgr = ts.IID_ITfCategoryMgr;
+const CLSID_TF_CategoryMgr = ts.CLSID_TF_CategoryMgr;
 pub fn createCategoryManager() ?*ITfCategoryMgr {
-    return com.createInstanceInproc(ITfCategoryMgr, com.CLSID_TF_CategoryMgr);
+    var result: ?*ITfCategoryMgr = null;
+    _ = CoCreateInstance(&CLSID_TF_CategoryMgr, null, CLSCTX_INPROC_SERVER, IID_ITfCategoryMgr, @ptrCast(&result));
+    return result;
 }
 
 pub const GUID_TFCAT_DISPLAYATTRIBUTEPROVIDER: Guid = Guid.initString("046b8c80-1647-40f7-9b21-b93b81aabc1b");
